856345123be0f087c17872dda29b89cafbedee94
Wouter <wouter@woutervandijke.nl>
2022-12-28 10:00:59 +0100

    first commit

--- hparams.py
+++ hparams.py
@@ -1,15 +1,17 @@
 import tensorflow as tf
+from tensorboard.plugins.hparams import api as hp
+
 from text import symbols
 
 
 def create_hparams(hparams_string=None, verbose=False):
     """Create model hyperparameters. Parse nondefault from given string."""
 
-    hparams = tf.contrib.training.HParams(
+    hparams = hp.hparams(
         ################################
         # Experiment Parameters        #
         ################################
-        epochs=500,
+        # epochs=500,
         iters_per_checkpoint=1000,
         seed=1234,
         dynamic_loss_scaling=True,
@@ -86,10 +88,10 @@ def create_hparams(hparams_string=None, verbose=False):
     )
 
     if hparams_string:
-        tf.logging.info('Parsing command line hparams: %s', hparams_string)
+        tf.compat.v1.logging.info('Parsing command line hparams: %s', hparams_string)
         hparams.parse(hparams_string)
 
     if verbose:
-        tf.logging.info('Final parsed hparams: %s', hparams.values())
+        tf.compat.v1.logging.info('Final parsed hparams: %s', hparams.values())
 
     return hparams
